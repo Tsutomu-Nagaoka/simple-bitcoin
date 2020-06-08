4286d5c3f64bcd0e32fe5d5d29415e72886ead1a
Tsutomu-Nagaoka <mxx.2963@icloud.com>
2020-06-09 02:39:50 +0900

    first commit

---
+++ client.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	mySocket, err := net.Dial("tcp", "10.1.1.27:50030")
+	if err != nil {
+		fmt.Printf("Dial error: %s\n", err)
+		return
+	}
+	defer mySocket.Close()
+
+	sendMsg := "Hello! This is test message from my sample client!"
+	mySocket.Write([]byte(sendMsg))
+}
